05683879a2d7fd0c6f9d7eb07203336f60fb5c81
Mr-Zer0 <bulletson.geek@gmail.com>
2015-07-09 19:03:23 +0630

    Add required function to candidate controller.

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -10,7 +10,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Candidate;
+use App\Transformers\CandidateTransformer;
 
-class CandidateController {
+class CandidateController extends Controller
+{
+    /**
+     * Get single candidate
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($id)
+    {
+        $data = (new Candidate())->find($id);
 
+        $item = $this->transform($data, new CandidateTransformer(), false);
+
+        return response_ok($item);
+    }
+
+    /**
+     * Get candidate list
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function candidateList()
+    {
+        $data = $this->transform($this->query(), new CandidateTransformer(), true);
+
+        return response_ok($data);
+    }
+
+    protected function query()
+    {
+        $model = new Candidate();
+
+        // Filter gender
+        if ($gender = app('request')->input('gender'))
+        {
+            $model = $model->where('gender', $gender);
+        }
+
+        // Filter religion
+        if ($religion = app('request')->input('religion'))
+        {
+            $model = $model->where('religion', $religion);
+        }
+
+        // Filter legislature
+        if ($legislature = app('request')->input('legislature'))
+        {
+            $model = $model->where('legislature', $legislature);
+        }
+
+        // Filter party
+        if ($party = app('request')->input('party'))
+        {
+            $model = $model->where('party_id', $party);
+        }
+
+        return $model->paginate();
+    }
 }
